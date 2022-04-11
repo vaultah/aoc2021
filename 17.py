5c42e0ab81d40f2d28f4ac991737eb72efe38fd6
vaultah <4944562+vaultah@users.noreply.github.com>
2022-04-11 23:22:43 +0100

    Day 17

---
+++ 17.py
@@ -0,0 +1,30 @@
+import re
+
+
+def count_shots(xmin, xmax, ymin, ymax):
+    count = 0
+
+    for vxi in range(1, xmax + 1):
+        for vyi in range(ymin, abs(ymin) + 1):
+            x, vx, y, vy = 0, vxi, 0, vyi
+
+            while y >= ymin and x <= xmax:
+                x += vx
+                y += vy
+
+                if vx > 0:
+                    vx -= 1
+
+                vy -= 1
+
+                if xmin <= x <= xmax and ymin <= y <= ymax:
+                    count += 1
+                    break
+
+    return count
+
+
+if __name__ == '__main__':
+    xmin, xmax, ymin, ymax = map(int, re.findall(r'-?\d+', input()))
+    print((ymin + 1) * ymin // 2)
+    print(count_shots(xmin, xmax, ymin, ymax))
